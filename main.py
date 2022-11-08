a29a24ba828f388d880c4e5270ec24c07ea8f604
Om Honrao <99074324+1nv1sibl3@users.noreply.github.com>
2022-11-08 20:53:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+from discord.ext import commands
+from discord import Intents
+
+from settings import *
+from webserver import keep_alive
+bot = commands.Bot(command_prefix="i.", intents=Intents.all())
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py") and filename != "__init__.py":
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.run("MTAyMzkyNDc1OTM3OTA2NjkwMA.GLMMBd.JI4vX77Ssa6XQiCU4UTHOfkONndxt9kHXRSgZM")
